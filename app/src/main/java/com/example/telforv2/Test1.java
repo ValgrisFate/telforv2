3c7286c0dd4bac8f564c3a4d203a796823300c68
ValgrisFate <inurufu12@gmail.com>
2022-09-15 17:32:05 -0700

    proyecto bueno

---
+++ app/src/main/java/com/example/telforv2/Test1.java
@@ -0,0 +1,19 @@
+package com.example.telforv2;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class Test1 extends AppCompatActivity {
+
+    private FloatingActionButton floatingActionButton;
+    RecyclerView recyclerView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test1);
+    }
+}
